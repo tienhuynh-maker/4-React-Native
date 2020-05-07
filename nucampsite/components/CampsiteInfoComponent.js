e1ddd3549f92368e4d2f0318b7a7174b1f5f69c4
Tien Huynh <tienhuynh234@gmail.com>
2020-05-07 17:02:23 -0500

    Gestures Part 2

--- nucampsite/components/CampsiteInfoComponent.js
+++ nucampsite/components/CampsiteInfoComponent.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList, Button, 
-    Modal, StyleSheet, Alert, PanResponder } from 'react-native';
+import {
+    Text, View, ScrollView, FlatList, Button,
+    Modal, StyleSheet, Alert, PanResponder
+} from 'react-native';
 import { Card, Rating, Input, Icon } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
@@ -24,10 +26,16 @@ function RenderCampsite(props) {
 
     const { campsite } = props;
 
-    const recognizeDrag = ({dx}) => (dx < -200) ? true : false;
+    const view = React.createRef();
+
+    const recognizeDrag = ({ dx }) => (dx < -200) ? true : false;
 
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
+        onPanResponderGrant: () => {
+            view.current.rubberBand(1000)
+                .then(endState => console.log(endState.finished ? 'finished' : 'canceled'));
+        },
         onPanResponderEnd: (e, gestureState) => {
             console.log('pan responder end', gestureState);
             if (recognizeDrag(gestureState)) {
@@ -55,11 +63,12 @@ function RenderCampsite(props) {
 
     if (campsite) {
         return (
-            <Animatable.View 
-            animation='fadeInDown' 
-            duration={2000} 
-            delay={1000}
-            {...panResponder.panHandlers}>
+            <Animatable.View
+                animation='fadeInDown'
+                duration={2000}
+                delay={1000}
+                ref={view}
+                {...panResponder.panHandlers}>
                 <Card
                     featuredTitle={campsite.name}
                     image={{ uri: baseUrl + campsite.image }}>
